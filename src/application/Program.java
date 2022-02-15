628b644347bc1869a9068089dc7ba17cbd0414ce
Aline Paz <aline.soares.2@hotmail.com>
2022-02-15 19:43:36 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Aline");
+		Department obj = new Department(1, "Tecnologico");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(1, "Aline", "aline@gmail.com", new Date(), 4000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
