9d84af508afe49ec55e32167238c9fc95b56b518
Aline Paz <aline.soares.2@hotmail.com>
2022-02-15 21:39:53 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,16 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Tecnologico");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(1, "Aline", "aline@gmail.com", new Date(), 4000.0, obj);
+		Seller seller = sellerDao.findById(1);
 		
 		System.out.println(seller);
 
