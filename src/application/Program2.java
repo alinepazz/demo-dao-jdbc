c0e117ee743c31c3d88a172cca965f8efcc5124b
Aline Paz <aline.soares.2@hotmail.com>
2022-02-22 06:11:44 -0300

    delete implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TESTE 1: department insert ===");
+		
+		Department department = new Department(null, "Beleza");
+		departmentDao.insert(department);
+		System.out.println("Inserted! New id = " +  department.getId());
+		
+		sc.close();
+
+	}
+
+}
